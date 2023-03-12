42b1aa6b28a81dcf20e9a66a9ad45b9e89c4dd84
sofiasketch <Shushakova1004@gmail.com>
2023-03-12 12:51:30 +0400

    Add culture

---
+++ PowerCollections/PowerCollections.Tests/CultureInitializer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerCollections.Tests
+{
+    [TestClass]
+    public class CultureInitializer
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+        }
+    }
+}
